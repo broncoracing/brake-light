d501fa5e79d8c8ce309341b2dcd68e31707d9dc5
Jack LeFevre <jackflef@gmail.com>
2022-03-07 20:33:24 -0500

    Added startup animation and made config changes to run on hardware

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -19,18 +19,7 @@ PwmOut led(LED1); // Debug led from nucleo board
 PwmOut led(PA_8); // Transistor for brake light LED
 #endif
 
-#ifdef DRS_ENABLED
-// Initialize the servos as outputs
-// The configuration for the Nucleo-F445RE doesn't support PWM on pins PA2 and PA3 in MBed.
-// This is not an issue for the f412 we use on the actual board.
-#define NUM_SERVOS 2
-PwmOut servos[NUM_SERVOS] = {PwmOut(PA_0), PwmOut(PA_1)};
 
-// Servo positions when DRS is not active (positions in uS, most servos expect 1000-2000 uS range)
-int servo_pos_no_drs[NUM_SERVOS] = {1000, 1000};
-// Servo positions when DRS is active
-int servo_pos_drs[NUM_SERVOS] = {2000, 2000};
-#endif
 
 // Event queue for handling I/O triggered by an ISR (e.g. printing, sending CAN messages)
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
@@ -73,6 +62,7 @@ void steering_wheel_received(CANMessage& msg) {
 
 // Handle CAN frames from the brake pressure sensor
 void brake_pressure_received(CANMessage& msg) {
+    led.period_ms(2);
     unsigned char pressure = msg.data[BRAKE_PRESSURE_BYTE];
     if(pressure > BRAKE_LIGHT_PRESSURE_CUTOFF) {
         led.write(brightness);
@@ -125,6 +115,19 @@ int main()
         servo.period_ms(20); // Most RC servos expect a 20ms period, but this can be sped up for some servos for a faster response
     }
 #endif
+
+    // Startup animation
+    for(float i = 0; i < 1; i += 0.01) {
+        led.write(i * i);
+        ThisThread::sleep_for(20ms);
+    }
+    for(float i = 1; i > 0; i -= 0.01) {
+        led.write(i * i);
+        ThisThread::sleep_for(20ms);
+    }
+    // Blink slowly until CAN data is received
+    led.write(0.1);
+    led.period_ms(500);
 
     // Start the event queue
     t.start(callback(&queue, &EventQueue::dispatch_forever));
--- firmware/main.h
+++ firmware/main.h
@@ -19,6 +19,19 @@
 
 // Uncomment to enable DRS servos.
 #define DRS_ENABLED
+#ifdef DRS_ENABLED
+// Initialize the servos as outputs
+// The configuration for the Nucleo-F445RE doesn't support PWM on pins PA2 and PA3 in MBed.
+// This is not an issue for the f412 we use on the actual board.
+#define NUM_SERVOS 4
+PwmOut servos[NUM_SERVOS] = {PwmOut(PA_0), PwmOut(PA_1), PwmOut(PA_2), PwmOut(PA_3)};
+
+// Servo positions when DRS is not active (positions in uS, most servos expect 1000-2000 uS range)
+int servo_pos_no_drs[NUM_SERVOS] = {1000, 1000, 1000, 1000};
+// Servo positions when DRS is active
+int servo_pos_drs[NUM_SERVOS] = {2000, 2000, 2000, 2000};
+#endif
+
 
 // Uncomment to use the LED on the Nucleo dev board instead of the LED pin on the brakelight board.
 //#define NUCLEO_LED
